68e464e3dc8307dd1a9631a1589cbd037127ab4c
IkklemanDevs <201242330+IkklemanDevs@users.noreply.github.com>
2025-07-16 20:36:56 +0800

    Additional dashboard content

--- drillmate/js/charts/weeklyPerformance.js
+++ drillmate/js/charts/weeklyPerformance.js
@@ -1,4 +1,7 @@
 import { Chart } from "chart.js/auto";
+import { Colors } from 'chart.js';
+
+Chart.register(Colors);
 
 export function initWeeklyPerformance () {
     console.log('Weekly bar chart loaded');
@@ -11,8 +14,13 @@ export function initWeeklyPerformance () {
     data: {
       labels: ['Sun', 'Mon', 'Tue', 'Web', 'Thur', 'Fri', 'Sat'],
       datasets: [{
-        label: 'Weekly Performance (m)',
-        data: ['128.3','48.1','141.1'],
+        label: 'Current Week\'s Performance (m)',
+        data: ['128.3','48.1','141.1','120.3','148.1','141.1','100.2'],
+        borderWidth: 1
+      },
+    {
+        label: 'Last Week\'s Performance (m)',
+        data: ['123.4', '84.7', '96.1', '141.6', '109.3', '77.8', '132.0'],
         borderWidth: 1
       }]
     },
