0784f6bd45c80a66f129932205e0eaae0eafb22c
IkklemanDevs <201242330+IkklemanDevs@users.noreply.github.com>
2025-07-18 21:02:25 +0800

    Worked on Drill Log form

--- drillmate/js/charts/weeklyPerformance.js
+++ drillmate/js/charts/weeklyPerformance.js
@@ -3,25 +3,36 @@ import { Colors } from 'chart.js';
 
 Chart.register(Colors);
 
-export function initWeeklyPerformance () {
-    console.log('Weekly bar chart loaded');
-};
+export function initWeeklyPerformance() {
+  console.log('Weekly bar chart loaded');
 
   const ctx = document.getElementById('weeklyPerformance');
+
+  if (!ctx) {
+    console.warn("Canvas not found");
+    return;
+  }
 
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Sun', 'Mon', 'Tue', 'Web', 'Thur', 'Fri', 'Sat'],
-      datasets: [{
-        label: 'Current Week\'s Performance (m)',
-        data: ['128.3','48.1','141.1','120.3','148.1','141.1','100.2'],
-        borderWidth: 1
-      },
-    {
-        label: 'Last Week\'s Performance (m)',
-        data: ['123.4', '84.7', '96.1', '141.6', '109.3', '77.8', '132.0'],
-        borderWidth: 1
-      }]
+      labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+      datasets: [
+        {
+          label: "Current Week's Performance (m)",
+          data: ['128.3','48.1','141.1','120.3','148.1','141.1','100.2'],
+          borderWidth: 1
+        },
+        {
+          label: "Last Week's Performance (m)",
+          data: ['123.4', '84.7', '96.1', '141.6', '109.3', '77.8', '132.0'],
+          borderWidth: 1
+        }
+      ]
     },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    }
   });
+}
